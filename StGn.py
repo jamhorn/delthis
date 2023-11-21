9469a7e50226b3eb4a94eea8ef3ef1e2032357cb
jamhorn <moneyappsus@gmail.com>
2023-11-21 15:34:03 +0100

    Update StGn.py

--- StGn.py
+++ StGn.py
@@ -18,7 +18,7 @@ def WriteToLogFile(data):
 def WaitUntilNextMorning():
     #Wait until tomorrow 00:00 am
     t = time.localtime()
-    # t = time.mktime(t[:3] + (0,0,0) + t[6:])
+    t = time.mktime(t[:3] + (0,0,0) + t[6:])
     secondsUntilMidnight = t + 24*3600 - time.time()
     # secondsUntilMidnight += 60
     WriteToLogFile("Pausing the bot until the next day (pausing for this long: '" + str(datetime.timedelta(seconds=secondsUntilMidnight)) + "')")
